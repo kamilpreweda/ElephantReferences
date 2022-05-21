41cc2015e9a01544ae135ba3b740802e43a6da70
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-21 12:22:24 +0200

    Added "WhoAmI " method Created simple console UI Added "Reference switch" logic

---
+++ ElephantReferences/Elephant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElephantReferences
+{
+    public class Elephant
+    {
+        public string Name;
+        public int earSize;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine($"Na imię mi {Name}.");
+            Console.WriteLine($"Długość moich uszu to {earSize}cm.");
+        }
+    }
+}
--- ElephantReferences/Program.cs
+++ ElephantReferences/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using ElephantReferences;
+
+Elephant lucek = new Elephant() { Name = "Lucek", earSize = 100 };
+Elephant laura = new Elephant() { Name = "Laura", earSize = 80 };
+
+
+Console.WriteLine("Wciśnij 1 (Lucek), 2 (Laura), 3(przestawienie)");
+
+while (true)
+{
+    char userInput = Console.ReadKey(true).KeyChar;
+    Console.WriteLine($"Wcisnąłeś {userInput}.");
+
+    if (userInput == '1')
+    {
+        Console.WriteLine("Wywołanie lucek.WhoAmI().");
+        lucek.WhoAmI();
+
+    }
+    else if (userInput == '2')
+    {
+        Console.WriteLine("Wywołanie laura.WhoAmI().");
+        laura.WhoAmI();
+
+    }
+    else if (userInput == '3')
+    {
+        Elephant holder;
+        holder = lucek;
+        lucek = laura;
+        laura = holder;
+        Console.WriteLine("Zmienne zostały przestawione");
+
+
+    }
+    else return;
+    Console.WriteLine();
+
+    
+}
+
+    
